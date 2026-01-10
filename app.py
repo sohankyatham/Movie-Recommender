e5b540d9f3700a574dff05ff95388d0a37e10eef
sohankyatham <Sohan.Kyatham23@gmail.com>
2026-01-10 15:13:12 -0500

    Use expander to collapse dataset

--- app.py
+++ app.py
@@ -2,14 +2,15 @@ import streamlit as st
 import pandas as pd
 
 st.title("Movie Recommender")
-st.write("Here is the IMDB Top 1000 movies dataset:")
+st.write("IMDb Top 1000 Movies")
 
 movies = pd.read_csv('imdb_top_1000.csv')
-st.write(movies)
+with st.expander("Click here to view the entire dataset", expanded=False):
+    st.dataframe(movies)
 
 # Select the number of movies you want to get recommended
 number_of_movies = st.slider("Use the slider value to show only N movies you want to get recommended", 1, 1000)
 
 # Show only the first N rows of the 'movies dataframe'
 st.write("You selected "+ str(number_of_movies) + " movies to be displayed")
-st.write(movies.head(number_of_movies+1))
+st.write(movies.head(number_of_movies))
