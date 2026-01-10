a2902b1af4585fceba171d40ad2154fe410fe08e
sohankyatham <Sohan.Kyatham23@gmail.com>
2026-01-10 15:06:50 -0500

    Display N movies based on user selection

--- app.py
+++ app.py
@@ -6,3 +6,10 @@ st.write("Here is the IMDB Top 1000 movies dataset:")
 
 movies = pd.read_csv('imdb_top_1000.csv')
 st.write(movies)
+
+# Select the number of movies you want to get recommended
+number_of_movies = st.slider("Use the slider value to show only N movies you want to get recommended", 1, 1000)
+
+# Show only the first N rows of the 'movies dataframe'
+st.write("You selected "+ str(number_of_movies) + " movies to be displayed")
+st.write(movies.head(number_of_movies+1))
